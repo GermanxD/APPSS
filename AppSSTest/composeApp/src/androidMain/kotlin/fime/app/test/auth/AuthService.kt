359221cf6ce0a560a750983934acd173b770db0c
German Bustos Moreno <germanthegamer1@outlook.com>
2024-12-17 19:38:11 -0600

    cambio de archivos

--- AppSSTest/composeApp/src/androidMain/kotlin/fime/app/test/auth/AuthService.kt
+++
@@ -1,7 +0,0 @@
-package fime.app.test.auth
-
-class AuthService {
-    fun login(username: String, password: String): Boolean {
-        return username == "user" && password == "password" // Lógica de ejemplo
-    }
-}
--- AppSSTest/composeApp/src/iosMain/kotlin/fime/app/test/MainViewController.kt
+++
@@ -1,5 +0,0 @@
-package fime.app.test
-
-import androidx.compose.ui.window.ComposeUIViewController
-
-fun MainViewController() = ComposeUIViewController { App() }
