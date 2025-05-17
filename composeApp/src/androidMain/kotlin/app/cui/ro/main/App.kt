2710c9a30bc04219553265c1a4f578de10ae2681
German Bustos <52357914+GermanxD@users.noreply.github.com>
2025-05-16 20:25:12 -0600

    se arregla problema con el appbar del dispositivo

--- composeApp/src/androidMain/kotlin/app/cui/ro/main/App.kt
+++ composeApp/src/androidMain/kotlin/app/cui/ro/main/App.kt
@@ -1,57 +1,75 @@
 package app.cui.ro.main
 
 import android.content.Context
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBars
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import app.cui.ro.auth.AuthService
+import app.cui.ro.models.Screen
 import app.cui.ro.navigation.NavBarScreenStart
 import app.cui.ro.ui.session.LoginScreen
 import app.cui.ro.ui.session.RegisterScreen
-import app.cui.ro.models.Screen
+import app.cui.ro.ui.theme.CuiroTheme
 
 @Composable
-fun App(
-    context: Context
-) {
-    val navController: NavHostController = rememberNavController()
+fun App(context: Context) {
+    CuiroTheme {
+        val navController: NavHostController = rememberNavController()
+        val authService = AuthService()
 
-    val authService = AuthService()
-    val startDestination = if (authService.isUserLoggedIn()) {
-        Screen.Home.route
-    } else {
-        Screen.Login.route
-    }
+        val startDestination = if (authService.isUserLoggedIn()) {
+            Screen.Home.route
+        } else {
+            Screen.Login.route
+        }
 
-    NavHost(navController = navController, startDestination = startDestination) {
-        composable(Screen.Login.route) {
-            LoginScreen(
-                onLoginSuccess = {
-                    authService.saveLoginState(context, true)
-                    navController.navigate(Screen.Home.route) {
-                        popUpTo(0)
+        Scaffold(
+            contentWindowInsets = WindowInsets.systemBars
+        ) { innerPadding ->
+            Box(
+                modifier = Modifier.padding(innerPadding)
+            ) {
+                NavHost(navController = navController, startDestination = startDestination) {
+                    composable(Screen.Login.route) {
+                        LoginScreen(
+                            onLoginSuccess = {
+                                authService.saveLoginState(context, true)
+                                navController.navigate(Screen.Home.route) {
+                                    popUpTo(0)
+                                }
+                            },
+                            onRegisterClicked = {
+                                navController.navigate(Screen.Register.route)
+                            }
+                        )
                     }
-                },
-                onRegisterClicked = {
-                    navController.navigate(Screen.Register.route)
+                    composable(Screen.Register.route) {
+                        RegisterScreen(
+                            onRegisterSuccess = {
+                                authService.saveLoginState(context, true)
+                                navController.navigate(Screen.Home.route) {
+                                    popUpTo(0)
+                                }
+                            },
+                            onBackClicked = {
+                                navController.popBackStack()
+                            }
+                        )
+                    }
+                    composable(Screen.Home.route) {
+                        NavBarScreenStart()
+                    }
                 }
-            )
-        }
-        composable(Screen.Register.route) {
-            RegisterScreen(
-                onRegisterSuccess = {
-                    authService.saveLoginState(context, true)
-                    navController.navigate(Screen.Home.route) {
-                        popUpTo(0)
-                    }
-                },
-                onBackClicked = { navController.popBackStack() }
-            )
-        }
-        composable(Screen.Home.route) {
-            NavBarScreenStart()
+            }
         }
     }
 }
+
--- composeApp/src/androidMain/kotlin/app/cui/ro/main/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/app/cui/ro/main/MainActivity.kt
@@ -3,14 +3,16 @@ package app.cui.ro.main
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.view.WindowCompat
 import com.google.firebase.FirebaseApp
 
 class MainActivity : ComponentActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         FirebaseApp.initializeApp(this)
+
+        WindowCompat.setDecorFitsSystemWindows(window, false)
 
         setContent {
             App(context = this)
